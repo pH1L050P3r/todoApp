b79e9c7c8a6a344665352ed487945980a4a48673
Siddharth Baudh <siddharth.baudh@finoit.com>
2023-02-17 13:16:06 +0530

    Added Dynamic Environment loading from .env file

---
+++ cmd/web/config.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+
+	"github.com/spf13/viper"
+)
+
+type Environment struct {
+	ENVIRONMENT string
+
+	WEB_HOST string
+	WEB_PORT string
+
+	RDS_USERNAME       string
+	RDS_PASSWORD       string
+	RDS_HOSTNAME       string
+	RDS_PORT           string
+	RDS_DB_NAME        string
+	RDS_CONNECT_STRING string
+}
+
+type AppConfig struct {
+	ENVIRONMENT Environment
+}
+
+func (Config *AppConfig) Init() {
+	InitEnvironment(&Config.ENVIRONMENT)
+}
+
+func InitEnvironment(e *Environment) {
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(".env not found in environment")
+	}
+
+	value, ok := viper.Get("ENVIRONMENT").(string)
+	if !ok {
+		log.Printf("DEBUG : ENVIRONMENT variable not found")
+		log.Printf("DEBUG : ENVIRONMENT set to test")
+		e.ENVIRONMENT = "test"
+	} else {
+		e.ENVIRONMENT = value
+	}
+
+	var database_variables = []string{
+		"RDS_USERNAME",
+		"RDS_PASSWORD",
+		"RDS_HOSTNAME",
+		"RDS_PORT",
+		"RDS_DB_NAME",
+	}
+	for _, variable := range database_variables {
+		value, ok = viper.Get(variable).(string)
+		if !ok {
+			log.Fatal(fmt.Printf("%s is not found", variable))
+		} else {
+			reflect.ValueOf(e).Elem().FieldByName(variable).SetString(value)
+			log.Printf("%s : %s", variable, value)
+		}
+	}
+
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var AppSettings AppConfig
+	AppSettings.Init()
+
+	log.Println(AppSettings.ENVIRONMENT)
+
 	server := gin.Default()
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Server is running",
 		})
 	})
-	server.Run(":8000")
+
+	serverURL := "0.0.0.0:8000"
+	server.Run(serverURL)
 }
